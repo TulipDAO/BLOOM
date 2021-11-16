ba7f1de5d47bab5fa33ac632f74f47816d202ffe
CryptoBlocksNFT <90620567+CryptoBlocksNFT@users.noreply.github.com>
2021-11-16 00:47:00 -0600

    Add files via upload

---
+++ Bloom.sol
@@ -0,0 +1,371 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "./Auth.sol";
+import "./IDEXFactory.sol";
+import "./IDEXRouter.sol";
+import "./IDividendDistributor.sol";
+import "./DividendDistributor.sol";
+
+/**
+ * @title Bloom token part of TulipDao
+ * https://thetulipdao.com
+ */
+contract Bloom is IERC20Metadata, Auth, Pausable {
+    using SafeMath for uint256;
+
+    uint256 public constant MASK = type(uint128).max;
+    address BEETS = 0xF24Bcf4d1e507740041C9cFd2DddB29585aDCe1e;
+    address public wFTM = 0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83;
+    address DEAD = 0x000000000000000000000000000000000000dEaD;
+    address ZERO = 0x0000000000000000000000000000000000000000;
+    address DEAD_NON_CHECKSUM = 0x000000000000000000000000000000000000dEaD;
+
+    string constant _name = "Bloom";
+    string constant _symbol = "BLOOM";
+    uint8 constant _decimals = 9;
+
+    uint256 _totalSupply = 100_000_000 * (10 ** _decimals);
+
+    mapping (address => uint256) _balances;
+    mapping (address => mapping (address => uint256)) _allowances;
+
+    mapping (address => bool) isFeeExempt;
+    mapping (address => bool) isDividendExempt;
+
+    uint256 liquidityFee = 400;
+    uint256 buybackFee = 100;
+    uint256 reflectionFee = 100;
+    uint256 devFee = 100;
+    uint256 totalFee = 700;
+    uint256 feeDenominator = 10000;
+
+    address public devFeeAddress;
+    address public autoLiquidityReceiver;
+
+    uint256 targetLiquidity = 25;
+    uint256 targetLiquidityDenominator = 100;
+
+    IDEXRouter public router;
+    address public pair;
+
+    uint256 public launchedAt;
+    uint256 public launchedAtTimestamp;
+
+    uint256 buybackMultiplierNumerator = 200;
+    uint256 buybackMultiplierDenominator = 100;
+    uint256 buybackMultiplierTriggeredAt;
+    uint256 buybackMultiplierLength = 30 minutes;
+
+    bool public autoBuybackEnabled = false;
+    mapping (address => bool) buyBacker;
+    uint256 autoBuybackCap;
+    uint256 autoBuybackAccumulator;
+    uint256 autoBuybackAmount;
+    uint256 autoBuybackBlockPeriod;
+    uint256 autoBuybackBlockLast;
+
+    DividendDistributor distributor;
+    address public distributorAddress;
+
+    uint256 distributorGas = 500000;
+
+    uint256 public swapThreshold = 5;
+    bool inSwap;
+    modifier swapping() { inSwap = true; _; inSwap = false; }
+
+    constructor (address _dexRouter) Auth(msg.sender) {
+        router = IDEXRouter(_dexRouter);
+        pair = IDEXFactory(router.factory()).createPair(wFTM, address(this));
+        _allowances[address(this)][address(router)] = _totalSupply;
+        wFTM = router.WETH();
+        distributor = new DividendDistributor(_dexRouter);
+        distributorAddress = address(distributor);
+
+        isFeeExempt[msg.sender] = true;
+        isDividendExempt[pair] = true;
+        isDividendExempt[address(this)] = true;
+        isDividendExempt[DEAD] = true;
+        buyBacker[msg.sender] = true;
+
+        autoLiquidityReceiver = msg.sender;
+        devFeeAddress = msg.sender;
+
+        approve(_dexRouter, _totalSupply);
+        approve(address(pair), _totalSupply);
+        _balances[msg.sender] = _totalSupply;
+        emit Transfer(address(0), msg.sender, _totalSupply);
+    }
+
+    receive() external payable {}
+
+    function totalSupply() external view override returns (uint256) { return _totalSupply; }
+    function decimals() external pure override returns (uint8) { return _decimals; }
+    function symbol() external pure override returns (string memory) { return _symbol; }
+    function name() external pure override returns (string memory) { return _name; }
+    function getOwner() external view returns (address) { return owner; }
+    modifier onlyBuybacker() { require(buyBacker[msg.sender] == true, ""); _; }
+    function balanceOf(address account) public view override returns (uint256) { return _balances[account]; }
+    function allowance(address holder, address spender) external view override returns (uint256) { return _allowances[holder][spender]; }
+
+    function approve(address spender, uint256 amount) public override returns (bool) {
+        _allowances[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
+        return true;
+    }
+
+    function approveMax(address spender) external returns (bool) {
+        return approve(spender, _totalSupply);
+    }
+
+    function transfer(address recipient, uint256 amount) external override returns (bool) {
+        return _transferFrom(msg.sender, recipient, amount);
+    }
+
+    function transferFrom(address sender, address recipient, uint256 amount) external override returns (bool) {
+        if(_allowances[sender][msg.sender] != _totalSupply){
+            _allowances[sender][msg.sender] = _allowances[sender][msg.sender].sub(amount, "Insufficient Allowance");
+        }
+        return _transferFrom(sender, recipient, amount);
+    }
+
+    function _transferFrom(address sender, address recipient, uint256 amount) internal returns (bool) {
+        if(paused()) {
+            require(isAuthorized(sender) || sender == pair || sender == address(router) || sender == distributorAddress, "Paused");
+        }
+
+        if(inSwap){ return _basicTransfer(sender, recipient, amount); }
+
+        if(shouldSwapBack()){ swapBack(); }
+        if(shouldAutoBuyback()){ triggerAutoBuyback(); }
+
+        _balances[sender] = _balances[sender].sub(amount, "Insufficient Balance");
+
+        uint256 amountReceived = shouldTakeFee(sender) ? takeFee(sender, recipient, amount) : amount;
+
+        _balances[recipient] = _balances[recipient].add(amountReceived);
+
+        if(!isDividendExempt[sender]){ try distributor.setShare(sender, _balances[sender]) {} catch {} }
+        if(!isDividendExempt[recipient]){ try distributor.setShare(recipient, _balances[recipient]) {} catch {} }
+
+        try distributor.process(distributorGas) {} catch {}
+
+        emit Transfer(sender, recipient, amountReceived);
+        return true;
+    }
+
+    function _basicTransfer(address sender, address recipient, uint256 amount) internal returns (bool) {
+        _balances[sender] = _balances[sender].sub(amount, "Insufficient Balance");
+        _balances[recipient] = _balances[recipient].add(amount);
+        // emit Transfer(sender, recipient, amount);
+        return true;
+    }
+
+    function shouldTakeFee(address sender) internal view returns (bool) {
+        return !isFeeExempt[sender];
+    }
+
+    function getTotalFee(bool selling) public view returns (uint256) {
+        if(launchedAt + 1 >= block.number) { return feeDenominator.sub(1); }
+        if(selling) { return getMultipliedFee(); }
+        return totalFee;
+    }
+
+    function getMultipliedFee() public view returns (uint256) {
+        if (launchedAtTimestamp + 1 days > block.timestamp) {
+            return totalFee.mul(18000).div(feeDenominator);
+        } else if (buybackMultiplierTriggeredAt.add(buybackMultiplierLength) > block.timestamp) {
+            uint256 remainingTime = buybackMultiplierTriggeredAt.add(buybackMultiplierLength).sub(block.timestamp);
+            uint256 feeIncrease = totalFee.mul(buybackMultiplierNumerator).div(buybackMultiplierDenominator).sub(totalFee);
+            return totalFee.add(feeIncrease.mul(remainingTime).div(buybackMultiplierLength));
+        }
+        return totalFee;
+    }
+
+    function takeFee(address sender, address receiver, uint256 amount) internal returns (uint256) {
+        uint256 feeAmount = amount.mul(getTotalFee(receiver == pair)).div(feeDenominator);
+
+        _balances[address(this)] = _balances[address(this)].add(feeAmount);
+        emit Transfer(sender, address(this), feeAmount);
+
+        return amount.sub(feeAmount);
+    }
+
+    function shouldSwapBack() internal view returns (bool) {
+        return msg.sender != pair
+        && !inSwap
+        && _balances[address(this)] >= swapThreshold;
+    }
+
+    function shouldAutoBuyback() internal view returns (bool) {
+        return msg.sender != pair
+            && !inSwap
+            && autoBuybackEnabled
+            && autoBuybackBlockLast + autoBuybackBlockPeriod <= block.number // After N blocks from last buyback
+            && address(this).balance >= autoBuybackAmount;
+    }
+
+    function castMerlinCandle(uint256 amount, bool triggerBuybackMultiplier) external authorized {
+    buyTokens(amount, DEAD);
+    if(triggerBuybackMultiplier){
+        buybackMultiplierTriggeredAt = block.timestamp;
+        emit BuybackMultiplierActive(buybackMultiplierLength);
+        }
+    }
+
+    function clearBuybackMultiplier() external authorized {
+        buybackMultiplierTriggeredAt = 0;
+    }
+
+    function triggerAutoBuyback() internal {
+        buyTokens(autoBuybackAmount, DEAD);
+        autoBuybackBlockLast = block.number;
+        autoBuybackAccumulator = autoBuybackAccumulator.add(autoBuybackAmount);
+        if(autoBuybackAccumulator > autoBuybackCap){ autoBuybackEnabled = false; }
+    }
+
+    function buyTokens(uint256 amount, address to) internal swapping {
+        address[] memory path = new address[](2);
+        path[0] = wFTM;
+        path[1] = address(this);
+        router.swapExactETHForTokensSupportingFeeOnTransferTokens{value: amount}(0, path, to, block.timestamp);
+    }
+
+    function setAutoBuybackSettings(bool _enabled, uint256 _cap, uint256 _amount, uint256 _period) external authorized {
+        autoBuybackEnabled = _enabled;
+        autoBuybackCap = _cap;
+        autoBuybackAccumulator = 0;
+        autoBuybackAmount = _amount;
+        autoBuybackBlockPeriod = _period;
+        autoBuybackBlockLast = block.number;
+    }
+
+    function setBuybackMultiplierSettings(uint256 numerator, uint256 denominator, uint256 length) external authorized {
+        require(numerator / denominator <= 2 && numerator > denominator);
+        buybackMultiplierNumerator = numerator;
+        buybackMultiplierDenominator = denominator;
+        buybackMultiplierLength = length;
+    }
+
+    function launched() internal view returns (bool) {
+        return launchedAt != 0;
+    }
+
+    function launch() public authorized {
+        require(launchedAt == 0, "Already launched");
+        launchedAt = block.number;
+        launchedAtTimestamp = block.timestamp;
+    }
+
+    function setIsDividendExempt(address holder, bool exempt) external authorized {
+        require(holder != address(this) && holder != pair);
+        isDividendExempt[holder] = exempt;
+        if(exempt) {
+            distributor.setShare(holder, 0);
+        } else {
+            distributor.setShare(holder, _balances[holder]);
+        }
+    }
+
+    function setIsFeeExempt(address holder, bool exempt) external authorized {
+        isFeeExempt[holder] = exempt;
+    }
+
+    function setFees(uint256 _liquidityFee, uint256 _buybackFee, uint256 _reflectionFee, uint256 _devFee, uint256 _feeDenominator) external authorized {
+        liquidityFee = _liquidityFee;
+        buybackFee = _buybackFee;
+        reflectionFee = _reflectionFee;
+        devFee = _devFee;
+        totalFee = _liquidityFee.add(_buybackFee).add(_reflectionFee).add(_devFee);
+        feeDenominator = _feeDenominator;
+        require(totalFee < feeDenominator/4);
+    }
+
+    function setFeeReceivers(address _autoLiquidityReceiver) external authorized {
+        autoLiquidityReceiver = _autoLiquidityReceiver;
+    }
+
+    function setSwapBackSettings(uint256 _amount) external authorized {
+        swapThreshold = _amount;
+    }
+
+    function setTargetLiquidity(uint256 _target, uint256 _denominator) external authorized {
+        targetLiquidity = _target;
+        targetLiquidityDenominator = _denominator;
+    }
+
+    function setDistributionCriteria(uint256 _minPeriod, uint256 _minDistribution) external authorized {
+        distributor.setDistributionCriteria(_minPeriod, _minDistribution);
+    }
+
+    function setDistributorSettings(uint256 gas) external authorized {
+        require(gas < 750000);
+        distributorGas = gas;
+    }
+
+    function getCirculatingSupply() public view returns (uint256) {
+        return _totalSupply.sub(balanceOf(DEAD)).sub(balanceOf(ZERO));
+    }
+
+    function getLiquidityBacking(uint256 accuracy) public view returns (uint256) {
+        return accuracy.mul(balanceOf(pair).mul(2)).div(getCirculatingSupply());
+    }
+
+    function isOverLiquified(uint256 target, uint256 accuracy) public view returns (bool) {
+        return getLiquidityBacking(accuracy) > target;
+    }
+
+    event AutoLiquify(uint256 amountFTM, uint256 amountBEETS);
+    event BuybackMultiplierActive(uint256 duration);
+
+    function swapBack() internal swapping {
+        uint256 dynamicLiquidityFee = isOverLiquified(targetLiquidity, targetLiquidityDenominator) ? 0 : liquidityFee;
+        uint256 amountToLiquify = swapThreshold.mul(dynamicLiquidityFee).div(totalFee).div(2);
+        uint256 amountToSwap = swapThreshold.sub(amountToLiquify);
+
+        address[] memory path = new address[](2);
+        path[0] = address(this);
+        path[1] = wFTM;
+        uint256 balanceBefore = address(this).balance;
+
+        router.swapExactTokensForETHSupportingFeeOnTransferTokens(amountToSwap, 0, path, address(this), block.timestamp);
+
+        uint256 amountFTM = address(this).balance.sub(balanceBefore);
+
+        uint256 totalFTMFee = totalFee.sub(dynamicLiquidityFee.div(2));
+
+        uint256 amountFTMLiquidity = amountFTM.mul(dynamicLiquidityFee).div(totalFTMFee).div(2);
+        uint256 amountFTMReflection = amountFTM.mul(reflectionFee).div(totalFTMFee);
+
+        try distributor.deposit{value: amountFTMReflection}() {} catch {}
+
+        if(amountToLiquify > 0){
+            router.addLiquidityETH{value: amountFTMLiquidity}(
+            address(this),
+            amountToLiquify, 0, 0, autoLiquidityReceiver, block.timestamp);
+            emit AutoLiquify(amountFTMLiquidity, amountToLiquify);
+        }
+
+        // devFee
+        uint256 devFeeAmount = amountFTM.mul(devFee).div(feeDenominator);
+        if(devFeeAmount > 0) {
+            (bool sent,) = payable(devFeeAddress).call{value: devFeeAmount}("");
+            require(sent, "Failed to send balance");
+        }
+    }
+
+    function setDevFeeAddress(address addr) public authorized {
+        devFeeAddress = addr;
+    }
+
+    function pause() public authorized {
+        _pause();
+    }
+
+    function unpause() public authorized {
+        _unpause();
+    }
+}
