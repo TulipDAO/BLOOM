8421dd02e43e2f1e659b5bcfb130fec0c52723b6
CryptoBlocksNFT <90620567+CryptoBlocksNFT@users.noreply.github.com>
2021-11-16 00:48:20 -0600

    Update DividendDistributor.sol

--- DividendDistributor.sol
+++ DividendDistributor.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "../token/ERC20/IERC20.sol";
-import "../utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "./IDEXRouter.sol";
 import "./IDividendDistributor.sol";
 
